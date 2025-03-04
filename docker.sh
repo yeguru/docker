c8ba2f7639c1362ddf6cee8b47a1601a36c92f37
yeguru <yr947987@gmail.com>
2025-03-04 19:40:32 +0530

    dockerfiles

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 growpart /dev/nvme0n1 4
 lvextend -l +50%FREE /dev/RootVG/rootVol
 lvextend -l +50%FREE /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
-
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
--- docker.tf
+++ docker.tf
@@ -1,23 +1,53 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
   instance_type          = "t3.micro"
+
+  # 20GB is not enough
+  root_block_device {
+    volume_size = 50  # Set root volume size to 50GB
+    volume_type = "gp3"  # Use gp3 for better performance (optional)
+  }
+ 
+  provisioner "file" {
+    source      = "docker.sh"
+    destination = "/tmp/docker.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/docker.sh ",
+      "sudo sh /tmp/docker.sh "
+    ]
+  }
+
+  connection {
+    host = aws_instance.this.public_ip
+  # host = self.public_ip
+    type = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+  }  
+
+  #user_data = file("docker.sh")
+
   tags = {
     Name    = "docker"
-    Purpose = "docker-practice"
   }
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "allow_all_docker" {
+  name        = "allow_all_docker"
   description = "Allow TLS inbound traffic and all outbound traffic"
+
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -34,4 +64,7 @@ resource "aws_security_group" "allow_tls" {
   tags = {
     Name = "allow_tls"
   }
+}
+output "docker_ip" {
+  value       = aws_instance.this.public_ip
 }
