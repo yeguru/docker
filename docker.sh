49141dacce589047073ad1f5d4ac0dfe46db85b3
yeguru <yr947987@gmail.com>
2025-03-09 11:11:21 +0530

    dockerfiles

--- docker.sh
+++ docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
 growpart /dev/nvme0n1 4
 lvextend -l +50%FREE /dev/RootVG/rootVol
 lvextend -l +50%FREE /dev/RootVG/varVol
@@ -10,3 +13,13 @@ dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+# kubectl installation
+CMD ["node","index.js"]curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+#eksctl installatrion
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+mv /tmp/eksctl /usr/local/bin/
+
